a9b70bbf4203f4ea63d9e14527ae38c60d5ce9e8
TemplateRise <safikuldeveloper@gmail.com>
2025-01-30 23:59:54 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+  $(".hero-carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      animateOut: 'fadeOut',
+      dots: false
+  });
+
+  var owl = $('.testimonial-carousel').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  dots: true,
+  responsive:{
+      0:{
+          items: 1
+      },
+      768:{
+          items: 2
+      },
+      1200:{
+          items: 3
+      }
+  }
+});
+
+// Custom Navigation
+$('.custom-next').click(function() {
+  owl.trigger('next.owl.carousel');
+});
+$('.custom-prev').click(function() {
+  owl.trigger('prev.owl.carousel');
+});
+
+});
